48e2bc76bce133ebfb59b522568a57459d4259d7
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-04-04 17:50:51 -0400

    added api_key getter

--- hvz.py
+++ hvz.py
@@ -5,6 +5,9 @@
 import click
 import os
 import requests
+import sys
+
+api_key = None
 
 @click.group()
 def main():
@@ -17,6 +20,13 @@ def api_set(key):
     key_file = os.path.join(os.getenv("HOME"), ".hvz.api")
     with open(key_file, "w") as f_hnd:
         f_hnd.write(key)
+
+def api_get():
+    key_file = os.path.join(os.getenv("HOME"), ".hvz.api")
+    with open(key_file, "r") as f_hnd:
+        api_key = r_hnd.read().strip()
+    if api_key is None:
+        sys.exit(1)
 
 @main.command()
 def rules():
